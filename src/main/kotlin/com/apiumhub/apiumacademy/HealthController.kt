a73537dea93195acd94d83cbb4238ed912917fc1
Diego Ojeda Garcia <diego.ojeda@nestle.com>
2024-04-25 17:41:03 +0200

    Create health controlle

---
+++ src/main/kotlin/com/apiumhub/apiumacademy/HealthController.kt
@@ -0,0 +1,10 @@
+package com.apiumhub.apiumacademy
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @GetMapping("/health")
+    fun health() = "Hello world from SpringBoot!"
+}
