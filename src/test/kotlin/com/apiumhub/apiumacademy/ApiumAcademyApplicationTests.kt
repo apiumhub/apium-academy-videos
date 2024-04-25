4ff0aa73053688208c4517323b1e4e8dbe6160b3
Diego Ojeda Garcia <diego.ojeda@nestle.com>
2024-04-25 16:59:36 +0200

    Initial commit

---
+++ src/test/kotlin/com/apiumhub/apiumacademy/ApiumAcademyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.apiumhub.apiumacademy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiumAcademyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
