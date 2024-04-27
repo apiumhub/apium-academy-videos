2a086c1056a2be8f510d57c5f049de4f2455b508
diego <diego.ojeda@apiumhub.com>
2024-04-27 16:41:36 +0200

    Run tests when commit test

--- src/test/kotlin/com/apiumhub/apiumacademy/infrastructure/repositories/CourseRepositoryTest.kt
+++ src/test/kotlin/com/apiumhub/apiumacademy/infrastructure/repositories/CourseRepositoryTest.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.assertDoesNotThrow
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
+
 @SpringBootTest
 class CourseRepositoryTest {
 
