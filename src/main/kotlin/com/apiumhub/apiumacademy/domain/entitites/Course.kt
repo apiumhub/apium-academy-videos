14e17251001e7297b88e6dfa0c4744e66a22a357
erik <erikvasilyan08@gmail.com>
2024-05-28 14:02:46 +0200

    register student, teacher

--- src/main/kotlin/com/apiumhub/apiumacademy/domain/entitites/Course.kt
+++ src/main/kotlin/com/apiumhub/apiumacademy/domain/entitites/Course.kt
@@ -1,12 +1,15 @@
 package com.apiumhub.apiumacademy.domain.entitites
 
 import com.apiumhub.apiumacademy.domain.entitites.modules.Module
+import com.apiumhub.apiumacademy.domain.exceptions.StudentNotInCourseException
 import com.apiumhub.apiumacademy.domain.exceptions.StudentsInCourseLimitReachedException
 import com.apiumhub.apiumacademy.domain.valueobjects.course.courseId.CourseId
 import com.apiumhub.apiumacademy.domain.valueobjects.course.courseName.CourseName
 import com.apiumhub.apiumacademy.domain.valueobjects.course.courseName.CourseNameConverter
 import com.apiumhub.apiumacademy.domain.valueobjects.shared.positiveInteger.PositiveInteger
 import com.apiumhub.apiumacademy.domain.valueobjects.shared.positiveInteger.PositiveIntegerConverter
+import com.apiumhub.apiumacademy.domain.valueobjects.student.studentId.StudentId
+import com.apiumhub.apiumacademy.domain.valueobjects.teacher.teacherId.TeacherId
 import jakarta.persistence.*
 
 @Entity
@@ -20,6 +23,12 @@ class Course private constructor(
     @Convert(converter = PositiveIntegerConverter::class)
     private var currentRegisteredStudents: PositiveInteger = PositiveInteger(0)
 
+    @ElementCollection
+    private val registeredStudentsIds: MutableSet<StudentId> = mutableSetOf()
+
+    @ElementCollection
+    private val registeredTeacherIds: MutableSet<TeacherId> = mutableSetOf()
+
     @ManyToMany
     private val modules: MutableSet<Module> = mutableSetOf()
 
@@ -27,32 +36,33 @@ class Course private constructor(
         if (currentRegisteredStudents.value == maxRegisteredStudents.value)
             throw StudentsInCourseLimitReachedException(maxRegisteredStudents.value)
         currentRegisteredStudents++
+        registeredStudentsIds.add(student.studentId)
         return Pair(this, CourseStudent.create(this, student))
     }
 
-    fun removeStudent(student: Student, courseStudent: CourseStudent): Course {
+    fun removeStudent(student: Student): Course {
+        if (!isStudentRegistered(student)) throw StudentNotInCourseException(student.studentId, courseId)
         currentRegisteredStudents--
+        registeredStudentsIds.remove(student.studentId)
         return this
     }
 
     fun isStudentRegistered(student: Student): Boolean {
-        TODO()
-//        return registeredStudentsIds.contains(student.id)
+        return registeredStudentsIds.contains(student.studentId)
     }
 
-    fun registerTeacher(teacher: Teacher) {
-        TODO()
-//        registeredTeacherIds.add(teacher.id)
+    fun registerTeacher(teacher: Teacher) : Course {
+        registeredTeacherIds.add(teacher.teacherId)
+        return this
     }
 
-    fun removeTeacher(teacher: Teacher) {
-        TODO()
-//        registeredTeacherIds.remove(teacher.id)
+    fun removeTeacher(teacher: Teacher) : Course {
+        registeredTeacherIds.remove(teacher.teacherId)
+        return this
     }
 
     fun isTeacherRegistered(teacher: Teacher): Boolean {
-        TODO()
-//        return registeredTeacherIds.contains(teacher.id)
+        return registeredTeacherIds.contains(teacher.teacherId)
     }
 
     companion object {
